7587b9ca1f3e3a23bba2184e57684c8e1556b3fa
Daniil <holodnayazvezda@yandex.ru>
2024-01-08 02:49:08 +0300

    FirstCommit

---
+++ src/main/java/com/example/vchatmessengerserver/VchatMessengerServerApplication.java
@@ -0,0 +1,11 @@
+package com.example.vchatmessengerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VchatMessengerServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VchatMessengerServerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/vchatmessengerserver/VchatMessengerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vchatmessengerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VchatMessengerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
