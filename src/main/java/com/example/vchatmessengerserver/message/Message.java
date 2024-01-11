3f20e5cc0d6c681b045aca5e1562fc4692041f50
Daniil <holodnayazvezda@yandex.ru>
2024-01-12 01:45:20 +0300

    Message class added

--- src/main/java/com/example/vchatmessengerserver/message/Message.java
+++ src/main/java/com/example/vchatmessengerserver/message/Message.java
@@ -1,9 +1,16 @@
 package com.example.vchatmessengerserver.message;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.example.vchatmessengerserver.group.Group;
+import com.example.vchatmessengerserver.user.User;
+
+import io.micrometer.common.lang.NonNull;
+import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Objects;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,6 +19,30 @@ import lombok.Setter;
 @Setter
 public class Message {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NonNull
+    @Column(length = 20000)
+    private String content;
+
+    @NonNull
+    @ManyToOne
+    private Group messageChat;
+
+    @NonNull
+    private ZonedDateTime creationDate;
+
+    @NonNull
+    @OneToOne
+    private User owner;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<User> readers = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Message &&
+                Objects.equals(((Message) obj).getId(), this.getId());
+    }
 }
